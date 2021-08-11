0beed0660bfba183068504cc283422626cba28d1
shuailong <shuailong@123.com>
2021-08-11 09:29:37 +0800

    hot-fix commit1

--- src/main/java/com/haust/gitdemo1.java
+++ src/main/java/com/haust/gitdemo1.java
@@ -4,5 +4,6 @@ public class gitdemo1 {
     public static void main(String[] args) {
         System.out.println("hello Git!");
         System.out.println("hello world!");
+        System.out.println("hello hot-fix!");
     }
 }
