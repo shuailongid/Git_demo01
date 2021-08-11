da2c36b1e9d52d9f12ae2e4491d67c0af77b660c
shuailong <shuailong@123.com>
2021-08-11 09:12:56 +0800

    2 commit

--- src/main/java/com/haust/gitdemo1.java
+++ src/main/java/com/haust/gitdemo1.java
@@ -3,5 +3,6 @@ package com.haust;
 public class gitdemo1 {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello world!");
     }
 }
